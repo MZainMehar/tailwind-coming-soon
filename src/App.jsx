257831eb8be4fb96ad924c0821c0ce508fa52b86
MZainMehar <mzainmehar-developer@outlook.com>
2024-05-13 21:38:56 +0500

    update landing page content

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ function App() {
           <div className="mt-12 text-white text-center">
             <h1 className="font-bold text-4xl md:text-5xl">Project Coming Soon</h1>
             <p className="pt-4 text-lg md:text-xl">
-              Story Gennie is an AI-powered platform that generates bedtime stories for children. Sign up to get notified when we launch.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
           </div>
           <SignUpForm />
