f3c5b6dfa488ea1a9294cf8ac2f558e9f39dfe61
MZainMehar <mzainmehar-developer@outlook.com>
2024-06-09 19:35:01 +0500

    Update countdown end date in CountdownTimer component

--- src/components/CountdownTimer.jsx
+++ src/components/CountdownTimer.jsx
@@ -10,7 +10,7 @@ function CountdownTimer() {
   });
 
   useEffect(() => {
-    const endTime = new Date('July 6, 2024 23:59:59 GMT+0530').getTime();
+    const endTime = new Date('August 24, 2024 23:59:59 GMT+0530').getTime();
     const interval = setInterval(() => {
       const now = new Date().getTime();
       const timeRemaining = endTime - now;
